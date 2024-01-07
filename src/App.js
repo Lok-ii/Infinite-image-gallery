7dd09858669d44824c21494c08431c32a71e589a
Lok-ii <lokeshkataria.work@gmail.com>
2024-01-07 17:54:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import Search from "./Components/Search/Search";
+import { ShimmerSimpleGallery } from "react-shimmer-effects";
 
 function App() {
+  const [randomDataArray, setRandomData] = useState([]);
+  const [searchDataArray, setSearchData] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    // const body = {
+    //   headers: {
+    //     Authorization: `Client-ID JLG2my6265GxYGDQzPO9sPhpU5uQHQq-z4_Z6QLCuM4`,
+    //   },
+    // };
+    const client_id = "JLG2my6265GxYGDQzPO9sPhpU5uQHQq-z4_Z6QLCuM4";
+    const firstData = async () => {
+      try {
+        const res = await axios.get(
+          `https://api.unsplash.com/photos/?client_id=${client_id}&page=${page}&per_page=20`
+        );
+        console.log(res.data);
+        if (page > 1) {
+          setRandomData([...randomDataArray, ...res.data]);
+        } else {
+          setRandomData(res.data);
+        }
+      } catch (error) {
+        console.log("le bhai tera error: ", error);
+      }
+    };
+    firstData();
+  }, [page]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.documentElement.scrollTop +
+          document.documentElement.clientHeight +
+          1 >=
+        document.documentElement.scrollHeight
+      ) {
+        setPage((prev) => prev + 1);
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Search setSearchData={setSearchData} />
+      <div className="results">
+        {randomDataArray.length !== 0 ? (
+          randomDataArray.map((item) => {
+            return (
+              <div className="imageContainer">
+                <img src={item["urls"]["regular"]} alt="" />
+                <div className="imageDetails">
+                  <p className="name">{item.user.first_name}</p>
+                  <p className="description">{item.description}</p>
+                  <p className="date">{item.created_at}</p>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <ShimmerSimpleGallery card imageHeight={300} caption />
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/Components/Search/Search.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import "./search.css";
+
+
+const Search = (props) => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [previousSeach, setPreviousSearch] = useState("");
+    const [searchCount, setSeachCount] = useState(0);
+
+    const searchImages = (e)=>{
+        e.preventDefault();
+        setSeachCount(prev => prev + 1);
+        setPreviousSearch(searchTerm);
+    }
+
+    const getSearch = (e)=>{
+        setSearchTerm(e.target.value);
+        
+    }
+
+    useEffect(()=>{
+        const fetchData = async ()=>{
+            try {
+                const response = await axios.get(
+                  `https://api.unsplash.com/search/photos?query=${searchTerm}`,
+                  {
+                    headers: {
+                      Authorization: `Client-ID JLG2my6265GxYGDQzPO9sPhpU5uQHQq-z4_Z6QLCuM4`,
+                    },
+                  }
+                );
+            
+                console.log(response.data.results);
+                if(searchTerm !== previousSeach){
+                    props.setSearchData((prev)=>{
+                        let newData = [...prev, ...response.data.results];
+                        return newData;
+                    });
+                }else{
+                    props.setSearchData(response.data.results)
+                }
+             } catch (error) {
+                console.error("le bhai tera error: ", error);
+             }
+        }
+        if(searchTerm !== ""){
+            fetchData();
+        }else{
+            props.setSearchData((prev)=> prev);
+        }
+    }, [searchCount]);
+
+  return (
+    <form className='searchForm' onSubmit={searchImages}>
+        <input type="text" placeholder='search kar na bhai' id='input-field' value={searchTerm} onChange={getSearch}/>
+        <button type='submit'>Search</button>
+    </form>
+  )
+}
+
+export default Search
